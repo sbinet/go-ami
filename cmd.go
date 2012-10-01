84e62e5c085533d03ec9af9f9b68b387d7df5808
Sebastien Binet <binet@cern.ch>
2012-10-01 18:27:30 +0200

    more doc for go-ami cmd

--- cmd.go
+++ cmd.go
@@ -43,7 +43,24 @@ func ami_make_cmd_cmd() *commander.Command {
 cmd sends an arbitrary AMI command to the AMI server.
 
 ex:
- $ go-ami cmd TCGetPackageInfo fullPackageName="/External/pyAMI" processingStep="production" project="TagCollector" repositoryName="AtlasOfflineRepository"
+
+$ go-ami cmd TCGetPackageInfo fullPackageName="/External/pyAMI" processingStep="production" project="TagCollector" repositoryName="AtlasOfflineRepository"
+
+row=0
+  -> computingDescription=0
+  -> created=2012-08-31 15:41:53 +0000 UTC
+  -> physicsDescription=
+  -> archive=0
+  -> groupOriginOfPackage=AtlasRelease
+  -> fullPackageName=/External/pyAMI
+  -> comment=
+  -> packageName=pyAMI
+  -> repositoryName=AtlasOfflineRepository
+  -> path=/External/
+  -> softwareDomain=
+  -> defaultType=leaf
+row=1
+ [...]
 `,
 		Flag: *flag.NewFlagSet("ami-cmd", flag.ExitOnError),
 	}
